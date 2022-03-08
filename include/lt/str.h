c94b89e8d156f9828aee872d730539a16376b8fa
Alex Edin <lutfisk@lutfisk.net>
2022-03-08 13:02:05 +0100

    Add lstr_startswith and lstr_endswith

--- include/lt/str.h
+++ include/lt/str.h
@@ -10,6 +10,22 @@ b8 lt_lstr_eq(lstr_t s1, lstr_t s2) {
 	return !((s1.len - s2.len) | memcmp(s1.str, s2.str, s1.len));
 }
 
+static LT_INLINE
+b8 lt_lstr_startswith(lstr_t str, lstr_t substr) {
+	if (str.len < substr.len)
+		return 0;
+	return memcmp(str.str, substr.str, substr.len) == 0;
+}
+
+static LT_INLINE
+b8 lt_lstr_endswith(lstr_t str, lstr_t substr) {
+	if (str.len < substr.len)
+		return 0;
+
+	char* end = str.str + str.len;
+	return memcmp(end - substr.len, substr.str, substr.len) == 0;
+}
+
 f64 lt_lstr_float(lstr_t);
 i64 lt_lstr_int(lstr_t);
 u64 lt_lstr_uint(lstr_t);
