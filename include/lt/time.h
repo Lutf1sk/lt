576f732bf5bc6c93540856769dceca306ac974ba
Alex Edin <lutfisk@lutfisk.net>
2022-09-04 03:00:16 +0200

    Add macros for converting between units of time

--- include/lt/time.h
+++ include/lt/time.h
@@ -3,6 +3,15 @@
 
 #include <lt/primitives.h>
 
+#define LT_SEC_TO_MSEC(n) ((n) * 1000)
+#define LT_SEC_TO_USEC(n) ((n) * 1000000)
+
+#define LT_MSEC_TO_SEC(n) ((n) / 1000)
+#define LT_MSEC_TO_USEC(n) ((n) * 1000)
+
+#define LT_USEC_TO_SEC(n) ((n) / 1000000)
+#define LT_USEC_TO_MSEC(n) ((n) / 1000)
+
 u64 lt_hfreq_time_usec(void);
 u64 lt_hfreq_time_msec(void);
 u64 lt_hfreq_time_sec(void);
