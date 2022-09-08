3f8e3137dba1099500fb18bff047955c0843940b
Alex Edin <lutfisk@lutfisk.net>
2022-09-08 05:04:57 +0200

    Un-break lt_str_printf :P

--- src/io/str.c
+++ src/io/str.c
@@ -2,19 +2,23 @@
 #include <lt/mem.h>
 
 static
-isz lt_str_io_callb(char* str, void* data, usz len) {
-	memcpy(str, data, len);
+isz lt_str_io_callb(char** str, void* data, usz len) {
+	memcpy(*str, data, len);
+	*str += len;
 	return len;
 }
 
 isz lt_str_vprintf(char* str, char* fmt, va_list argl) {
-	return lt_io_vprintf((lt_io_callback_t)lt_str_io_callb, str, fmt, argl);
+	char* it = str;
+	return lt_io_vprintf((lt_io_callback_t)lt_str_io_callb, &it, fmt, argl);
 }
 
 isz lt_str_printf(char* str, char* fmt, ...) {
+	char* it = str;
+
 	va_list argl;
 	va_start(argl, fmt);
-	isz bytes = lt_io_vprintf((lt_io_callback_t)lt_str_io_callb, str, fmt, argl);
+	isz bytes = lt_io_vprintf((lt_io_callback_t)lt_str_io_callb, &it, fmt, argl);
 	va_end(argl);
 	return bytes;
 }
