0ee9a347ce9f8396efe79d26dae56930a0114bde
Alex Edin <lutfisk@lutfisk.net>
2022-09-11 07:08:12 +0200

    Add an allocator interface for the LibC heap

--- src/mem/heap.c
+++ src/mem/heap.c
@@ -1,21 +1,32 @@
 #include <lt/mem.h>
 
+#if defined(LT_UNIX)
+#	include <malloc.h>
+#	define MSIZE(p) malloc_usable_size(p)
+#elif defined(LT_WINDOWS)
+#	include <malloc.h>
+#	define MSIZE(p) _msize(p)
+#endif
+
 static
-void* reserve(void* usr, usz size) {
+void* lt_libc_malloc(void* usr, usz size) {
 	return malloc(size);
 }
 
 static
-void relinq(void* usr, void* mem) {
+void lt_libc_free(void* usr, void* mem) {
 	free(mem);
 }
 
 static
-void* resize(void* usr, void* mem, usz size) {
+void* lt_libc_realloc(void* usr, void* mem, usz size) {
 	return realloc(mem, size);
 }
 
-lt_alloc_t lt_heap_interface(void) {
-	return LT_ALLOC_INTERFACE(NULL, reserve, relinq, resize);
+static
+usz lt_libc_msize(void* usr, void* mem) {
+	return MSIZE(mem);
 }
 
+static lt_alloc_t lt_libc_heap_ = LT_ALLOC_INTERFACE(lt_libc_malloc, lt_libc_free, lt_libc_realloc, lt_libc_msize);
+lt_alloc_t* lt_libc_heap = &lt_libc_heap_;
