410717c4a86f01dd833157987ca7da30bdf4f670
Alex Edin <lutfisk@lutfisk.net>
2022-12-20 16:31:05 +0100

    Fix upper range of C1 in lt_is_unicode_control_char

--- include/lt/ctype.h
+++ include/lt/ctype.h
@@ -80,12 +80,12 @@ u8 lt_to_lower(u8 c) {
 
 static LT_INLINE
 u8 lt_is_control_char(u8 c) {
-	return c < 32 || c == 0x7F;
+	return c < 0x20 || c == 0x7F;
 }
 
 static LT_INLINE
-u8 lt_is_unicode_control_char(u32 c) {
-	return (c < 32) || (c >= 0x7F && c < 0x100);
+b8 lt_is_unicode_control_char(u32 c) {
+	return (c < 0x20) || (c >= 0x7F && c <= 0x9F);
 }
 
 #endif
