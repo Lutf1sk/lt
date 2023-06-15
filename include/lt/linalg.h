fa45bf5698f9660b9dd22391fa0b68f79b58e49e
Alex Edin <lutfisk@lutfisk.net>
2023-06-15 05:08:40 +0200

    linalg: add quaternions

--- include/lt/linalg.h
+++ include/lt/linalg.h
@@ -8,6 +8,8 @@
 
 #include <math.h>
 
+// ----- Vector
+
 typedef
 union lt_vec2 {
 	struct { float x, y; };
@@ -43,6 +45,22 @@ union lt_vec4 {
 #define LT_VEC2(x, y) ((lt_vec2_t)LT_VEC2_INIT(x, y))
 #define LT_VEC3(x, y, z) ((lt_vec3_t)LT_VEC3_INIT(x, y, z))
 #define LT_VEC4(x, y, z, w) ((lt_vec4_t)LT_VEC4_INIT(x, y, z, w))
+
+// ----- Quaternion
+
+typedef
+union lt_quat {
+	struct { float x, y, z, w; };
+	float data[4];
+} lt_quat_t;
+
+#define LT_QUAT_INIT(x, y, z, w) { .data = { (x), (y), (z), (w) } }
+#define LT_QUAT(x, y, z, w) ((lt_quat_t)LT_QUAT_INIT(x, y, z, w))
+
+#define LT_QUAT_IDENTITY_INIT LT_QUAT_INIT(0.0f, 0.0f, 0.0f, 1.0f)
+#define LT_QUAT_IDENTITY ((lt_quat_t)LT_QUAT_IDENTITY_INIT)
+
+// ----- Matrix
 
 typedef
 union lt_mat2 {
@@ -602,79 +620,72 @@ int lt_v4equ(lt_vec4_t a, lt_vec4_t b, f32 epsilon) {
 		lt_vec4_t: lt_v4equ \
 	)((a), (b), (c)))
 
-// ----- Shortened names
+// ----- quat
 
-#ifdef LT_LINALG_SHORTEN_NAMES
-#	define vec2 lt_vec2_t
-#	define vec3 lt_vec3_t
-#	define vec4 lt_vec4_t
+static LT_INLINE
+lt_quat_t lt_qadd(lt_quat_t a, lt_quat_t b) {
+	return LT_QUAT(a.x + b.x, a.y + b.y, a.z + b.z, a.w + b.w);
+}
 
-#	define mat2 lt_mat2_t
-#	define mat3 lt_mat3_t
-#	define mat4 lt_mat4_t
+static LT_INLINE
+lt_quat_t lt_qsub(lt_quat_t a, lt_quat_t b) {
+	return LT_QUAT(a.x - b.x, a.y - b.y, a.z - b.z, a.w - b.w);
+}
 
-#	define vadd lt_vadd
-#	define vsub lt_vsub
-#	define vdiv lt_vdiv
-#	define vmul lt_vmul
-#	define vaddf lt_vaddf
-#	define vsubf lt_vsubf
-#	define vdivf lt_vdivf
-#	define vmulf lt_vmulf
-#	define vmulm lt_vmulm
-#	define vmulm4 lt_vmulm4
-#	define vmax lt_vmax
-#	define vmin lt_vmin
+static LT_INLINE
+lt_quat_t lt_qmul(lt_quat_t a, lt_quat_t b) {
+	return LT_QUAT(
+		a.w * b.x + a.x * b.w + a.y * b.z - a.z * b.y,
+		a.w * b.y + a.y * b.w + a.z * b.x - a.x * b.z,
+		a.w * b.z + a.z * b.w + a.x * b.y - a.y * b.x,
+		a.w * b.w - a.x * b.x - a.y * b.y - a.z * b.z
+	);
+}
 
-#	define vdot lt_vdot
-#	define vneg lt_vneg
-#	define vcross lt_vcross
-#	define vcrossn lt_vcrossn
-#	define vmag lt_vmag
-#	define vdist lt_vdist
-#	define vnorm lt_vnorm
-#	define vabs lt_vabs
-#	define vequ lt_vequ
+static LT_INLINE
+lt_quat_t lt_qaddf(lt_quat_t q, f32 f) {
+	return LT_QUAT(q.x + f, q.y + f, q.z + f, q.w + f);
+}
 
-#	define vproject lt_vproject
-#	define vpproject lt_vpproject
+static LT_INLINE
+lt_quat_t lt_qsubf(lt_quat_t q, f32 f) {
+	return LT_QUAT(q.x - f, q.y - f, q.z - f, q.w - f);
+}
 
-#	define m4perspective lt_m4perspective
-#	define m4ortho lt_m4ortho
-#	define m4view lt_m4view
-#	define m4euler lt_m4euler
-#	define m4translate lt_m4translate
-#	define m4scale lt_m4scale
-#	define m4model lt_m4model
-#	define m4inverse lt_m4inverse
+static LT_INLINE
+lt_quat_t lt_qdivf(lt_quat_t q, f32 f) {
+	return LT_QUAT(q.x / f, q.y / f, q.z / f, q.w / f);
+}
 
-#	define m2identity lt_m2identity
-#	define m3identity lt_m3identity
-#	define m4identity lt_m4identity
+static LT_INLINE
+lt_quat_t lt_qmulf(lt_quat_t q, f32 f) {
+	return LT_QUAT(q.x * f, q.y * f, q.z * f, q.w * f);
+}
 
-#	define m2mul lt_m2mul
-#	define m3mul lt_m3mul
-#	define m4mul lt_m4mul
-#endif
+static LT_INLINE
+f32 lt_qdot(lt_quat_t a, lt_quat_t b) {
+	return a.x * b.x + a.y * b.y + a.z * b.z + a.w * b.w;
+}
 
-// matrix.c
+static LT_INLINE
+lt_quat_t lt_qnorm(lt_quat_t q) {
+	f32 mag = sqrt(lt_qdot(q, q));
+	if (mag == 0.0f)
+		return LT_QUAT(0.0f, 0.0f, 0.0f, 0.0f);
+	return lt_qmulf(q, 1.0f / mag);
+}
+
+static LT_INLINE
+int lt_qequ(lt_quat_t a, lt_quat_t b, f32 epsilon) {
+	return fabs(a.x - b.x) < epsilon && fabs(a.y - b.y) < epsilon && fabs(a.z - b.z) < epsilon && fabs(a.w - b.w) < epsilon;
+}
+
+// ----- mat2
 
 static LT_INLINE
 void lt_m2identity(lt_mat2_t* mat) {
 	static lt_mat2_t src = LT_MAT2_IDENTITY_INIT;
 	memcpy(mat, &src, sizeof(lt_mat2_t));
-}
-
-static LT_INLINE
-void lt_m3identity(lt_mat3_t* mat) {
-	static lt_mat3_t src = LT_MAT3_IDENTITY_INIT;
-	memcpy(mat, &src, sizeof(lt_mat3_t));
-}
-
-static LT_INLINE
-void lt_m4identity(lt_mat4_t* mat) {
-	static lt_mat4_t src = LT_MAT4_IDENTITY_INIT;
-	memcpy(mat, &src, sizeof(lt_mat4_t));
 }
 
 static LT_INLINE
@@ -685,6 +696,14 @@ lt_mat2_t lt_m2mul(const lt_mat2_t* m1, const lt_mat2_t* m2) {
 	);
 }
 
+// ----- mat3
+
+static LT_INLINE
+void lt_m3identity(lt_mat3_t* mat) {
+	static lt_mat3_t src = LT_MAT3_IDENTITY_INIT;
+	memcpy(mat, &src, sizeof(lt_mat3_t));
+}
+
 static LT_INLINE
 lt_mat3_t lt_m3mul(const lt_mat3_t* m1, const lt_mat3_t* m2) {
 	return LT_MAT3V(
@@ -692,6 +711,14 @@ lt_mat3_t lt_m3mul(const lt_mat3_t* m1, const lt_mat3_t* m2) {
 		lt_v3mulm(m1->j, m2),
 		lt_v3mulm(m1->k, m2)
 	);
+}
+
+// ----- mat4
+
+static LT_INLINE
+void lt_m4identity(lt_mat4_t* mat) {
+	static lt_mat4_t src = LT_MAT4_IDENTITY_INIT;
+	memcpy(mat, &src, sizeof(lt_mat4_t));
 }
 
 static LT_INLINE
@@ -733,5 +760,71 @@ lt_mat4_t lt_m4scale(lt_vec3_t scale) {
 lt_mat4_t lt_m4model(lt_vec3_t pos, lt_vec3_t rot, lt_vec3_t scale);
 
 lt_mat4_t lt_m4inverse(const lt_mat4_t* m);
+
+// ----- Shortened names
+
+#ifdef LT_LINALG_SHORTEN_NAMES
+#	define vec2 lt_vec2_t
+#	define vec3 lt_vec3_t
+#	define vec4 lt_vec4_t
+
+#	define mat2 lt_mat2_t
+#	define mat3 lt_mat3_t
+#	define mat4 lt_mat4_t
+
+#	define vadd lt_vadd
+#	define vsub lt_vsub
+#	define vdiv lt_vdiv
+#	define vmul lt_vmul
+#	define vaddf lt_vaddf
+#	define vsubf lt_vsubf
+#	define vdivf lt_vdivf
+#	define vmulf lt_vmulf
+#	define vmulm lt_vmulm
+#	define vmulm4 lt_vmulm4
+#	define vmax lt_vmax
+#	define vmin lt_vmin
+
+#	define vdot lt_vdot
+#	define vneg lt_vneg
+#	define vcross lt_vcross
+#	define vcrossn lt_vcrossn
+#	define vmag lt_vmag
+#	define vdist lt_vdist
+#	define vnorm lt_vnorm
+#	define vabs lt_vabs
+#	define vequ lt_vequ
+
+#	define vproject lt_vproject
+#	define vpproject lt_vpproject
+
+#	define qadd lt_qmul
+#	define qsub lt_qsub
+#	define qmul lt_qmul
+#	define qaddf lt_qmulf
+#	define qsubf lt_qsubf
+#	define qdivf lt_qdivf
+#	define qmulf lt_qmulf
+#	define qdot lt_qdot
+#	define qnorm lt_qnorm
+#	define qequ lt_qequ
+
+#	define m4perspective lt_m4perspective
+#	define m4ortho lt_m4ortho
+#	define m4view lt_m4view
+#	define m4euler lt_m4euler
+#	define m4translate lt_m4translate
+#	define m4scale lt_m4scale
+#	define m4model lt_m4model
+#	define m4inverse lt_m4inverse
+
+#	define m2identity lt_m2identity
+#	define m3identity lt_m3identity
+#	define m4identity lt_m4identity
+
+#	define m2mul lt_m2mul
+#	define m3mul lt_m3mul
+#	define m4mul lt_m4mul
+#endif
 
 #endif
