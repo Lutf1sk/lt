1f77934cfae7a0ef50844735e1a232e7be0f77d4
Alex Edin <lutfisk@lutfisk.net>
2023-11-12 04:27:48 +0100

    arg: add fancy foreach macro to argument iterator

--- include/lt/arg.h
+++ include/lt/arg.h
@@ -15,6 +15,8 @@ struct lt_arg_iterator {
 lt_arg_iterator_t lt_arg_iterator_create(int argc, char** argv);
 b8 lt_arg_next(lt_arg_iterator_t* it);
 
+#define lt_foreach_arg(it_name, argc, argv) for ( lt_arg_iterator_t __it = lt_arg_iterator_create(argc, argv), *it_name = &__it; lt_arg_next(&__it); )
+
 b8 lt_arg_flag(lt_arg_iterator_t* it, char short_key, lstr_t long_key);
 b8 lt_arg_str(lt_arg_iterator_t* it, char short_key, lstr_t long_key, char** out);
 b8 lt_arg_lstr(lt_arg_iterator_t* it, char short_key, lstr_t long_key, lstr_t* out);
