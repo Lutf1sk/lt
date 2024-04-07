303b91ab64ef8694dd65d90d244c998b262f3500
Alex Edin <lutfisk@lutfisk.net>
2024-04-07 03:13:28 +0200

    io: add a buffer to io_vprintf

--- src/io/fmt.c
+++ src/io/fmt.c
@@ -1,13 +1,39 @@
 #include <lt/io.h>
 #include <lt/mem.h>
+#include <lt/str.h>
+#include <lt/math.h>
+
+#define BUFSZ 1024
+
+static LT_FLATTEN
+isz buffered_write(lt_io_callback_t callb, void* usr, char** it, char* end, lstr_t data) {
+	isz written = 0;
+	if (*it + data.len > end) {
+		char* start = end - BUFSZ;
+		usz buffered = *it - start;
+		*it = start;
+		written += callb(usr, start, buffered);
+	}
+
+	if (data.len >= BUFSZ) {
+		written += callb(usr, data.str, data.len);
+	}
+	else {
+		*it += lt_mcopy(*it, data.str, data.len);
+	}
+	return written;
+}
+
+
+#define NUM_MAX 64
 
 isz lt_io_dummy_callb(void* usr, void* data, usz len) {
 	return len;
 }
 
 static LT_FLATTEN LT_INLINE
-isz lt_io_printuq(lt_io_callback_t callb, void* usr, u64 n) {
-	char buf[32], *end = buf + sizeof(buf), *it = end - 1;
+isz lt_io_printuq(lt_io_callback_t callb, void* usr, char** buf_it, char* buf_end, u64 n) {
+	char buf[NUM_MAX], *end = buf + sizeof(buf), *it = end - 1;
 
 	// Fill buffer backwards with the remainder of n/10
 	while (n >= 10) {
@@ -15,32 +41,26 @@ isz lt_io_printuq(lt_io_callback_t callb, void* usr, u64 n) {
 		n /= 10;
 	}
 	*it = n + '0';
-
-	usz len = end - it;
-	return callb(usr, it, len);
+	return buffered_write(callb, usr, buf_it, buf_end, lt_lsfrom_range(it, end));
 }
 
 static LT_FLATTEN LT_INLINE
-isz lt_io_printiq(lt_io_callback_t callb, void* usr, i64 n) {
-	static char minus = '-';
-	usz sign = 0;
+isz lt_io_printiq(lt_io_callback_t callb, void* usr, char** buf_it, char* buf_end, i64 n) {
+	isz written = 0;
 
-	u64 v = n;
 	// Handle signed values
 	if (n < 0) {
-		v = -v;
-		callb(usr, &minus, 1);
-		sign = 1;
+		n = -n;
+		written += buffered_write(callb, usr, buf_it, buf_end, CLSTR("-"));
 	}
-
-	return lt_io_printuq(callb, usr, v) + sign;
+	return lt_io_printuq(callb, usr, buf_it, buf_end, n) + written;
 }
 
 static char io_hex_char[16] = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F' };
 
 static LT_FLATTEN LT_INLINE
-isz lt_io_printhq(lt_io_callback_t callb, void* usr, u64 n) {
-	char buf[32], *end = buf + sizeof(buf), *it = end - 1;
+isz lt_io_printhq(lt_io_callback_t callb, void* usr, char** buf_it, char* buf_end, u64 n) {
+	char buf[NUM_MAX], *end = buf + sizeof(buf), *it = end - 1;
 
 	// Fill buffer backwards with the lowest nibble of n
 	while (n > 0x0F) {
@@ -48,28 +68,24 @@ isz lt_io_printhq(lt_io_callback_t callb, void* usr, u64 n) {
 		n >>= 4;
 	}
 	*it = io_hex_char[n];
-
-	usz len = end - it;
-	return callb(usr, it, len);
+	return buffered_write(callb, usr, buf_it, buf_end, lt_lsfrom_range(it, end));
 }
 
 static LT_FLATTEN LT_INLINE
-isz lt_io_printbq(lt_io_callback_t callb, void* usr, u64 n) {
-	char buf[64], *end = buf + sizeof(buf), *it = end - 1;
+isz lt_io_printbq(lt_io_callback_t callb, void* usr, char** buf_it, char* buf_end, u64 n) {
+	char buf[NUM_MAX], *end = buf + sizeof(buf), *it = end - 1;
 
 	while (n > 1) {
 		*it-- = '0' + (n & 1);
 		n >>= 1;
 	}
-	*it =  '0' + (n & 1);
-
-	usz len = end - it;
-	return callb(usr, it, len);
+	*it = '0' + (n & 1);
+	return buffered_write(callb, usr, buf_it, buf_end, lt_lsfrom_range(it, end));
 }
 
 static LT_FLATTEN LT_INLINE
-isz lt_io_printmq(lt_io_callback_t callb, void* usr, u64 n) {
-	char buf[32] = "B";
+isz lt_io_printmq(lt_io_callback_t callb, void* usr, char** buf_it, char* buf_end, u64 n) {
+	char buf[NUM_MAX] = "B";
 	usz len = 1;
 
 	isz res, size = 0;
@@ -96,68 +112,70 @@ isz lt_io_printmq(lt_io_callback_t callb, void* usr, u64 n) {
 		n /= LT_KB(1);
 	}
 
-	if (buf[1] == ':') // !! this is a really dumb workaround for rounding errors, should be improved
+	if (buf[1] == ':') { // !! this is a really dumb workaround for rounding errors, should be improved
 		buf[1] = '9';
+	}
 
-	if ((res = lt_io_printuq(callb, usr, n)) < 0)
+	if ((res = lt_io_printuq(callb, usr, buf_it, buf_end, n)) < 0) {
 		return res;
+	}
 	size += res;
 
-	if ((res = callb(usr, buf, len)) < 0)
+	if ((res = buffered_write(callb, usr, buf_it, buf_end, LSTR(buf, len))) < 0) {
 		return res;
-	size += res;
-
-	return size;
+	}
+	return size + res;
 }
 
 static LT_INLINE
-isz lt_io_printfq(lt_io_callback_t callb, void* usr, f64 n) {
+isz lt_io_printfq(lt_io_callback_t callb, void* usr, char** buf_it, char* buf_end, f64 n) {
 	return -LT_ERR_UNSUPPORTED;
 }
 
-
 isz lt_io_vprintf(lt_io_callback_t callb, void* usr, char* fmt, va_list argl) {
-	char* it = fmt;
+	char buf[BUFSZ], *buf_it = buf, *buf_end = buf_it + BUFSZ;
 
 	isz written = 0;
 
 	char c;
-	while ((c = *it) != 0) {
+	while ((c = *fmt) != 0) {
 		if (c != '%') {
-			char* substr_start = it;
-			while ((c = *it) != 0 && c != '%')
-				++it;
-			written += callb(usr, substr_start, it - substr_start);
+			char* substr_start = fmt;
+			while ((c = *fmt) != 0 && c != '%') {
+				++fmt;
+			}
+
+			written += buffered_write(callb, usr, &buf_it, buf_end, lt_lsfrom_range(substr_start, fmt));
 			continue;
 		}
 
-		c = *++it;
+		c = *++fmt;
 
 		switch (c) {
-		case 0: --it; break;
+		case 0: --fmt; break;
 
 		case '%':
-			written += callb(usr, "%", 1);
+			written += buffered_write(callb, usr, &buf_it, buf_end, CLSTR("%"));
 			break;
 
 		case 'c': {
 			char c = va_arg(argl, int);
-			written += callb(usr, &c, 1);
+			written += buffered_write(callb, usr, &buf_it, buf_end, LSTR(&c, 1));
 		}	break;
 
 		case 'S': {
 			lstr_t str = va_arg(argl, lstr_t);
-			written += callb(usr, str.str, str.len);
+			written += buffered_write(callb, usr, &buf_it, buf_end, str);
 		}	break;
 
 		case 's': {
 			char* str = va_arg(argl, char*);
-			written += callb(usr, str, strlen(str));
+			written += buffered_write(callb, usr, &buf_it, buf_end, lt_lsfroms(str));
 		}	break;
 
 		case 'u': {
 			u64 val;
-			c = *++it;
+			c = *++fmt;
 			switch (c) {
 			case 'b': val = va_arg(argl, unsigned int); break;
 			case 'w': val = va_arg(argl, unsigned int); break;
@@ -166,12 +184,12 @@ isz lt_io_vprintf(lt_io_callback_t callb, void* usr, char* fmt, va_list argl) {
 			case 'z': val = va_arg(argl, usz); break;
 			default: val = 0; break;
 			}
-			written += lt_io_printuq(callb, usr, val);
+			written += lt_io_printuq(callb, usr, &buf_it, buf_end, val);
 		}	break;
 
 		case 'i': {
 			i64 val;
-			c = *++it;
+			c = *++fmt;
 			switch (c) {
 			case 'b': val = va_arg(argl, int); break;
 			case 'w': val = va_arg(argl, int); break;
@@ -180,12 +198,12 @@ isz lt_io_vprintf(lt_io_callback_t callb, void* usr, char* fmt, va_list argl) {
 			case 'z': val = va_arg(argl, isz); break;
 			default: val = 0; break;
 			}
-			written += lt_io_printiq(callb, usr, val);
+			written += lt_io_printiq(callb, usr, &buf_it, buf_end, val);
 		}	break;
 
 		case 'h': {
 			u64 val;
-			c = *++it;
+			c = *++fmt;
 			switch (c) {
 			case 'b': val = va_arg(argl, unsigned int); break;
 			case 'w': val = va_arg(argl, unsigned int); break;
@@ -194,12 +212,12 @@ isz lt_io_vprintf(lt_io_callback_t callb, void* usr, char* fmt, va_list argl) {
 			case 'z': val = va_arg(argl, usz); break;
 			default: val = 0; break;
 			}
-			written += lt_io_printhq(callb, usr, val);
+			written += lt_io_printhq(callb, usr, &buf_it, buf_end, val);
 		}	break;
 
 		case 'b': {
 			u64 val;
-			c = *++it;
+			c = *++fmt;
 			switch (c) {
 			case 'b': val = va_arg(argl, unsigned int); break;
 			case 'w': val = va_arg(argl, unsigned int); break;
@@ -208,12 +226,12 @@ isz lt_io_vprintf(lt_io_callback_t callb, void* usr, char* fmt, va_list argl) {
 			case 'z': val = va_arg(argl, usz); break;
 			default: val = 0; break;
 			}
-			written += lt_io_printbq(callb, usr, val);
+			written += lt_io_printbq(callb, usr, &buf_it, buf_end, val);
 		}	break;
 
 		case 'm': {
 			u64 val;
-			c = *++it;
+			c = *++fmt;
 			switch (c) {
 			case 'b': val = va_arg(argl, unsigned int); break;
 			case 'w': val = va_arg(argl, unsigned int); break;
@@ -222,40 +240,53 @@ isz lt_io_vprintf(lt_io_callback_t callb, void* usr, char* fmt, va_list argl) {
 			case 'z': val = va_arg(argl, usz); break;
 			default: val = 0; break;
 			}
-			written += lt_io_printmq(callb, usr, val);
+			written += lt_io_printmq(callb, usr, &buf_it, buf_end, val);
 		}	break;
 
 		case 'p':
-			written += lt_io_printhq(callb, usr, va_arg(argl, usz));
+			written += lt_io_printhq(callb, usr, &buf_it, buf_end, va_arg(argl, usz));
 			break;
 
 		case 'f': {
-			f64 val;
-			c = *++it;
-			switch (c) {
-			case 'd': val = va_arg(argl, double); break;
-			case 'q': val = va_arg(argl, f64); break;
-			default: val = 0.0f; break;
+			c = *++fmt;
+			if (c != 'd' && c != 'q') {
+				--fmt;
 			}
-			written += lt_io_printfq(callb, usr, val);
+			written += lt_io_printfq(callb, usr, &buf_it, buf_end, va_arg(argl, f64));
 		}	break;
 
 		case 'r': {
-			char val = *++it;
+			char val = *++fmt;
 			usz repeat = va_arg(argl, usz);
-			for (usz i = 0; i < repeat; ++i)
-				written += callb(usr, &val, 1);
+
+			for (;;) {
+				usz writable = lt_min_usz(repeat, buf_end - buf_it);
+				repeat -= writable;
+				buf_it += lt_mset8(buf_it, val, writable);
+
+				if (repeat > 0) {
+					written += callb(usr, buf, buf_it - buf);
+					buf_it = buf;
+				}
+				else {
+					break;
+				}
+			}
 		}	break;
 
 		default: {
 			char buf[2] = "% ";
 			buf[1] = c;
-			written += callb(usr, buf, 2);
+			written += buffered_write(callb, usr, &buf_it, buf_end, LSTR(buf, 2));
 		}	break;
 		}
-		++it;
+		++fmt;
 	}
 
+	usz buf_rem = buf_it - buf;
+	if (buf_rem > 0) {
+		written += callb(usr, buf, buf_rem);
+	}
 	return written;
 }
 
