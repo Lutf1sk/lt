6db7e6222bb456a3f7074bdb8957968a0b985deb
Alex Edin <lutfisk@lutfisk.net>
2025-02-02 21:09:23 +0100

    rand: add fnv hash

--- include/lt/rand.h
+++ include/lt/rand.h
@@ -20,4 +20,33 @@ u32 lt_pcg_hash(u32 seed) {
 	return (word >> 22u) ^ word;
 }
 
+#define LT_FNV_PRIME32 16777619
+#define LT_FNV_PRIME64 1099511628211
+
+#define LT_FNV_BASE32 2166136261
+#define LT_FNV_BASE64 14695981039346656037
+
+static LT_INLINE
+u32 lt_fnv1a_hashs(const char str[static 1]) {
+	u32 hash = LT_FNV_BASE32;
+	for (const char* it = str; *it; ++it) {
+		hash = (hash ^ (u32)*it) * LT_FNV_PRIME32;
+	}
+	return hash;
+}
+
+static LT_INLINE
+u32 lt_fnv1a_hashls(lstr_t str) {
+	u32 hash = LT_FNV_BASE32;
+	for (char* it = str.str, *end = it + str.len; it < end; ++it) {
+		hash = (hash ^ (u32)*it) * LT_FNV_PRIME32;
+	}
+	return hash;
+}
+
+static LT_INLINE
+u16 lt_hash16_from32(u32 hash) {
+	return (hash >> 16) ^ hash;
+}
+
 #endif
