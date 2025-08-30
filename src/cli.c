80868959d29e01cb77a91752ddaa2cfd7f3bc89a
Alex Edin <lutfisk@lutfisk.net>
2025-08-30 14:08:49 +0200

    remove useless cast in lprintf()

--- src/cli.c
+++ src/cli.c
@@ -7,10 +7,10 @@ isz write_out(void* usr, const void* data, usz size) {
 	return write((int)(isz)usr, data, size);
 }
 
-usz lprintf(const char* fmt, ...) {
+isz lprintf(const char* fmt, ...) {
 	va_list arg_list;
 	va_start(arg_list, fmt);
-	usz res = vlprintf((write_fn)write_out, (void*)STDOUT_FILENO, fmt, arg_list);
+	usz res = vlprintf(write_out, (void*)STDOUT_FILENO, fmt, arg_list);
 	va_end(arg_list);
 	return res;
 }
