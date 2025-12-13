21c851a513c2e3387a9aaf88cce7cb6a09a5fbe9
Alex Edin <lutfisk@lutfisk.net>
2025-12-13 16:08:15 +0100

    begin logging rewrite

--- include/lt2/log.h
+++ include/lt2/log.h
@@ -11,65 +11,84 @@
 #define LOG_INFO   6
 #define LOG_DEBUG  7
 
-typedef struct log_header {
-	u8  version;
-	u8  headings;
-	u16 header_size;
+#define LOGSINK_FILE    0
+#define LOGSINK_NETWORK 1
+#define LOGSINK_RINGBUF 2
 
+typedef struct log_sink {
+	u8 type;
+	union {
+		struct {
+			logfile_header* header;
+		} disk_ringbuf;
+
+		struct {
+			ls  host;
+			u16 port;
+			i32 sockfd;
+		} network;
+
+		struct {
+			i32 fd;
+			u8  color;
+		} file;
+	};
+	log_sink* next;
+	log_sink* fallback;
+} log_sink;
+
+// --- ringbuf
+
+typedef struct PACKED(logfile_header) {
+	u8 version;
+	u8 headings;
+	u8 pad[2];
+
+	u32 strtab_start;
+	u32 strtab_max;
 	u32 strtab_size;
-	u32 strtab_top;
 
-	u32 max_entries;
-	u32 enttab_first;
-	u32 entry_count;
-} log_header;
+	u32 enttab_start;
+	u32 enttab_max;
+	u32 enttab_count;
+} logfile_header;
 
-typedef struct log_entry {
+typedef struct PACKED(logfile_entry) {
 	u8  info;
-	u8  fields;
-	u8  pad[2];
+	u8  pad[1];
+	u16 fields;
 	u32 size;
-	u32 offset;
+	u64 offset;
 	u64 time;
-} log_entry;
+} logfile_entry;
 
-extern ls log_mapping;
-extern log_header* log_file_header;
+log_sink log_map_ringbuf(ls path, u8 mode);
+log_sink log_alloc_ringbuf(usz max_entry_count, usz max_strtab_size);
 
-extern u8* log_strtab;
-extern usz log_strtab_mask;
-
-extern log_entry* log_enttab;
-extern usz log_enttab_mask;
+// ---
 
 #define MAX_LOG_FIELDS 32
-
 extern thread_local usz log_field_count;
 extern thread_local u64 log_fields[MAX_LOG_FIELDS];
 
-b8 log_open(ls path, u8 mode, err* err);
-void log_close(err* err);
-
-void print_log_entry(i32 index);
-
 INLINE
-usz log_push_field(u64 val) {
+usz log_push(u64 val) {
 	if (log_field_count < MAX_LOG_FIELDS)
 		log_fields[log_field_count++] = val;
 	return log_field_count - 1;
 }
 
 INLINE
-void log_pop_field(void) {
+void log_pop(void) {
 	if (log_field_count)
 		--log_field_count;
 }
 
 INLINE
-void log_clear_fields(void) {
+void log_clear(void) {
 	log_field_count = 0;
 }
 
-i32 vlogf(u8 info, const char* fmt, va_list args);
-i32 llogf(u8 info, const char* fmt, ...);
+i32 vlogf(log_sink* sink, u8 info, const char* fmt, va_list args);
+i32 logf(log_sink* sink, u8 info, const char* fmt, ...);
 
--- src/log.c
+++ src/log.c
@@ -4,93 +4,6 @@
 
 #include <time.h>
 
-ls log_mapping;
-log_header* log_file_header;
-
-u8* log_strtab;
-usz log_strtab_mask;
-
-log_entry* log_enttab;
-usz log_enttab_mask;
-
-thread_local usz log_field_count = 0;
-thread_local u64 log_fields[MAX_LOG_FIELDS];
-
-b8 log_create(ls path, u32 max_entries, u32 strtab_size, err* err) {
-	if (!is_nzpow2(max_entries) || !is_nzpow2(strtab_size)) {
-		throw(err, ERR_BAD_ARGUMENT, "invalid table size(s)");
-		return 0;
-	}
-
-	file_handle f = fcreate(path, RW, err);
-	if (f < 0)
-		return 0;
-	if (!fsetsize(f, sizeof(log_header) + max_entries * sizeof(log_entry) + strtab_size, err)) {
-		lfclose(f, err_ignore);
-		return 0;
-	}
-
-	b8 success = lfwrite(f, &(log_header) {
-		.version = 0,
-		.header_size = sizeof(log_header),
-		.strtab_size = strtab_size,
-		.max_entries = max_entries,
-	}, sizeof(log_header), err);
-
-	lfclose(f, err);
-	return success;
-}
-
-b8 log_open(ls path, u8 mode, err* err) {
-	ls mapping;
-	if (mode & W) {
-		mapping = fmapall(path, mode, err_ignore);
-		if (!mapping.size) {
-			if (!log_create(path, MB(1), MB(32), err))
-				return 0;
-			mapping = fmapall(path, mode, err);
-		}
-	}
-	else {
-		mapping = fmapall(path, mode, err);
-	}
-	if (!mapping.size)
-		return 0;
-
-	if (mapping.size < sizeof(log_header)) {
-		throw(err, ERR_CORRUPT, "log file does not contain a valid header");
-		goto err0;
-	}
-
-	log_header* header_tmp = (log_header*)mapping.ptr;
-	if (header_tmp->version != 0 || header_tmp->header_size < sizeof(log_header)) {
-		throw(err, ERR_UNSUPPORTED, "log file version is unknown or invalid");
-		goto err0;
-	}
-
-	usz total_size = (usz)header_tmp->header_size + (usz)header_tmp->strtab_size + (usz)header_tmp->max_entries * sizeof(log_entry);
-	if (total_size > mapping.size || !is_nzpow2(header_tmp->strtab_size) || !is_nzpow2(header_tmp->max_entries)) {
-		throw(err, ERR_CORRUPT, "invalid table size(s)");
-		goto err0;
-	}
-
-	log_mapping     = mapping;
-	log_file_header = header_tmp;
-	log_strtab_mask = header_tmp->strtab_size - 1;
-	log_enttab_mask = header_tmp->max_entries - 1;
-	log_strtab      = (u8*)(log_mapping.ptr + header_tmp->header_size);
-	log_enttab      = (log_entry*)(log_strtab + header_tmp->strtab_size);
-	log_field_count = 0;
-	return 1;
-
-err0: funmap(mapping, err);
-	return 0;
-}
-
-void log_close(err* err) {
-	funmap(log_mapping, err);
-}
-
 static ls severity_names[] = {
 	[LOG_EMERG]  = ls("EMERG!\x1b[0;1m"),
 	[LOG_ALERT]  = ls("ALERT!\x1b[0;1m"),
@@ -113,110 +26,13 @@ static ls severity_prefixes[] = {
 	[LOG_DEBUG]  = ls("\x1b[37;1m"),
 };
 
-void print_log_entry(i32 index) {
-	log_entry* ent = &log_enttab[index];
-	u8* ent_str_start = log_strtab + ent->offset;
-	u8 sev = ent->info & 7;
-	usz fields_size = ent->fields * sizeof(*log_fields);
-	lprintf("{ls}{dt64} {ls} ", severity_prefixes[sev], ent->time, severity_names[sev]);
-	if (ent->fields) {
-		for (usz i = 0; i < ent->fields; ++i)
-			lprintf("\x1b[4;3{usz}m{u64}\x1b[0m ", (i & 3) + 3,((u64*)ent_str_start)[i]);
-	}
-	lprintf("{ls}\x1b[0m\n", lls(ent_str_start + fields_size, ent->size - fields_size - 1));
+
+log_sink log_map_ringbuf(ls path, u8 mode) {
+	
 }
 
-static
-i32 index_by_offset(u32 offs) {
-	if (!log_file_header->entry_count)
-		return -1;
-
-	u32 r = 0;
-	u32 l = log_file_header->entry_count - 1;
-	u8 above_top = offs >= log_file_header->strtab_top;
-	do {
-		u32 m = (r + l) >> 2;
-		log_entry* ent = &log_enttab[(log_file_header->enttab_first + m) & log_enttab_mask];
-		if (above_top == (ent->offset >= log_file_header->strtab_top)) {
-			if (offs < ent->offset) {
-				r = m;
-				continue;
-			}
-			if (offs < ent->offset + ent->size)
-				return m;
-			l = m;
-		}
-		else if (above_top) {
-			l = m;
-		}
-		else {
-			r = m;
-		}
-	} while (r != l);
-	return -1;
+log_sink log_alloc_ringbuf(usz max_entry_count, usz max_strtab_size) {
+	
 }
 
-i32 vlogf(u8 info, const char* fmt, va_list args) {
-	if (!log_file_header)
-		return -1;
-
-	va_list args2;
-	va_copy(args2, args);
-
-	usz fields_size = log_field_count * sizeof(*log_fields);
-	usz msglen = fields_size + vllenf(fmt, args) + 1;
-	if (msglen > log_file_header->strtab_size) {
-		throw(err_warn, ERR_LIMIT_EXCEEDED, "log entry exceeds maximum size");
-		va_end(args2);
-		return -1;
-	}
-
-	u32 advance = 0;
-	if (msglen + log_file_header->strtab_top > log_file_header->strtab_size) {
-		for (; advance < log_file_header->entry_count; ++advance) {
-			usz offs = log_enttab[(log_file_header->enttab_first + advance) & log_enttab_mask].offset;
-			if (offs < log_file_header->strtab_top)
-				break;
-		}
-		log_file_header->strtab_top = 0;
-	}
-
-	for (; advance < log_file_header->entry_count; ++advance) {
-		usz index = (log_file_header->enttab_first + advance) & log_enttab_mask;
-		usz offs = log_enttab[index].offset;
-		usz size = log_enttab[index].size;
-		if (log_file_header->strtab_top + msglen <= offs || log_file_header->strtab_top >= offs + size)
-			break;
-	}
-
-	memcpy(log_strtab + log_file_header->strtab_top, log_fields, fields_size);
-	u8* str_start = log_strtab + log_file_header->strtab_top + fields_size;
-	ls msg = vlsprintf(lls(str_start, msglen), fmt, args2);
-	va_end(args2);
-	str_start[msg.size] = 0;
-
-	u32 ent_index = (log_file_header->enttab_first + log_file_header->entry_count) & log_enttab_mask;
-	if (!advance && log_file_header->entry_count >= log_file_header->max_entries)
-		++advance;
-	log_file_header->entry_count += 1 - advance;
-	log_file_header->enttab_first = (log_file_header->enttab_first + advance) & log_enttab_mask;
-
-	log_enttab[ent_index] = (log_entry) {
-		.info   = info,
-		.fields = log_field_count,
-		.size   = msglen,
-		.offset = log_file_header->strtab_top,
-		.time   = time(NULL)
-	};
-	log_file_header->strtab_top += msglen;
-	return ent_index;
-}
-
-i32 llogf(u8 info, const char* fmt, ...) {
-	va_list args;
-	va_start(args, fmt);
-	i32 res = vlogf(info, fmt, args);
-	va_end(args);
-	return res;
-}
 
